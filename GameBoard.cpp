703d0f70aeb08a5a731b5516c1b7c12dd5d595cb
Ying-Coco <ywen1306@csu.fullerton.edu>
2016-11-10 17:29:28 -0800

    Update GameBoard.cpp

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -2,7 +2,7 @@
 //  GameBoard.cpp
 //
 
-#include "GameBoard.hpp"
+#include "GameBoard.h"
 
 // Build the gameboard
 // If the landing square is a chute, set the position to the corresponding lower square
@@ -12,6 +12,35 @@
 // Otherwise, player stays put at the landing square
 // TO DO: implement this method
 void GameBoard::buildBoard() {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+ //  // TODO: implement this function properly
+	//throw std::logic_error("not implemented yet");
+	
+	gmBoard.reserve(BOARD_SIZE);
+
+	for(int i=0; i<BOARD_SIZE; i++)
+		gmBoard[i]=i;
+	
+	//Ladders
+	gmBoard[1] = 38;
+	gmBoard[4] = 14;
+	gmBoard[9] = 31;
+	gmBoard[21] = 42;
+	gmBoard[28] = 84;
+	gmBoard[36] = 44;
+	gmBoard[51] = 67;
+	gmBoard[71] = 91;
+	gmBoard[80] = 100;
+
+	//Chutes
+	gmBoard[16] = 6;
+	gmBoard[47] = 26;
+	gmBoard[49] = 11;
+	gmBoard[56] = 53;
+	gmBoard[62] = 19;
+	gmBoard[64] = 60;
+	gmBoard[87] = 24;
+	gmBoard[93] = 73;
+	gmBoard[95] = 75;
+	gmBoard[98] = 78;
+	
 }
